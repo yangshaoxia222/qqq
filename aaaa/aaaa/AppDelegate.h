6c211282c64a1acad2676e5181a9ffc234961846
yangshaoxia222ini <540202350@qq.com>
2015-09-07 22:43:57 +0800

    hjhjafdsf

---
+++ aaaa/aaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaa
+//
+//  Created by yang on 15/9/7.
+//  Copyright (c) 2015年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaa/aaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaa
+//
+//  Created by yang on 15/9/7.
+//  Copyright (c) 2015年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
